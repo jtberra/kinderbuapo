3ab9a03084951093ac9070b0c76901d1634ee7fa
Angel Govi <79714714+angel-govi@users.noreply.github.com>
2021-10-28 10:30:04 -0500

    Update interfaz_profesor.php

--- interfaz_profesor.php
+++ interfaz_profesor.php
@@ -22,12 +22,6 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="#">About</a>
         </li>
         <li class="nav-item dropdown">
